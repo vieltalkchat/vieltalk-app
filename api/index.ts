45711ec852723676d343b88e93ce3aecb9eacf0a
Layhout <layhoutworkmail@gmail.com>
2025-10-03 15:17:46 +0700

    chore: get loggers

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,34 @@
-import axios, { AxiosResponse, CreateAxiosDefaults, InternalAxiosRequestConfig } from 'axios'
+import axios, { AxiosError, AxiosResponse, CreateAxiosDefaults, InternalAxiosRequestConfig } from 'axios'
 import Aes from 'react-native-aes-crypto'
 import AesGcmCrypto from 'react-native-aes-gcm-crypto'
+import 'react-native-get-random-values'
 import { RSA } from 'react-native-rsa-native'
+import { v4 as uuidv4 } from 'uuid'
 
-async function requestInterceptor(config: InternalAxiosRequestConfig) {
-  // TODO: encrypt request data
+type AxiosConfigWithMetadata = InternalAxiosRequestConfig & {
+  metadata?: {
+    axiosId: string
+  }
+}
+
+async function requestInterceptor(config: AxiosConfigWithMetadata) {
   // TODO: add token to request headers
+
+  const axiosId = uuidv4()
+
+  console.debug('outbound request', {
+    baseUrl: config.baseURL,
+    method: config.method,
+    data: config.data,
+    params: config.params,
+    url: config.url,
+    headers: config.headers,
+    axiosId,
+  })
 
   const body = config.data
 
-  if (body) {
+  if (body && !(body instanceof FormData)) {
     const key = await Aes.randomKey(32)
 
     const encrypted = await AesGcmCrypto.encrypt(JSON.stringify(body), true, key)
@@ -22,11 +41,22 @@ async function requestInterceptor(config: InternalAxiosRequestConfig) {
     }
   }
 
+  config.metadata = {
+    axiosId: axiosId,
+  }
+
   return config
 }
 
 function responseInterceptor(response: AxiosResponse) {
-  // TODO: logging and debugging
+  console.debug('outbound response success', {
+    baseUrl: response.config.baseURL,
+    url: response.config.url,
+    status: `${response.status}:${response.statusText}`,
+    headers: response.headers,
+    body: response.data,
+    axiosId: (response.config as AxiosConfigWithMetadata).metadata?.axiosId,
+  })
 
   return response
 }
@@ -35,11 +65,27 @@ function responseErrorInterceptor(error: any) {
   // TODO: refresh token
   // TODO: handle error
 
+  if (error instanceof Error) {
+    const axiosError = error as AxiosError
+    console.debug('outbound response failure', {
+      baseUrl: axiosError?.response?.config.baseURL,
+      url: axiosError?.response?.config.url,
+      status: axiosError.response?.status,
+      headers: axiosError.response?.headers,
+      body: axiosError.response?.data,
+      axiosId: (axiosError?.response?.config as AxiosConfigWithMetadata).metadata?.axiosId,
+    })
+  }
+
   return Promise.reject(error)
 }
 
+const defaultAxiosOptions: CreateAxiosDefaults = {
+  baseURL: process.env.EXPO_PUBLIC_API_BASE_URL,
+}
+
 export function createApiClient(options?: CreateAxiosDefaults) {
-  const instance = axios.create(options || { baseURL: process.env.EXPO_PUBLIC_API_BASE_URL })
+  const instance = axios.create(options || defaultAxiosOptions)
 
   instance.interceptors.request.use(requestInterceptor)
   instance.interceptors.response.use(responseInterceptor, responseErrorInterceptor)
